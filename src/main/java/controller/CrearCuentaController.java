e026c31fc3d9816ecc3fee25eebede9a7406cafc
Luis Marquinez <luisdmarquinez@gmail.com>
2021-10-11 12:46:16 -0500

    Interfaz CrearCuenta
    Corregida la interfaz de crear cuenta y su respectivo controlador.

---
+++ src/main/java/controller/CrearCuentaController.java
@@ -0,0 +1,44 @@
+package controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import java.awt.event.*;
+
+public class CrearCuentaController {
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private PasswordField txtUsername;
+
+    @FXML
+    private TextField txtApellido;
+
+    @FXML
+    private Button botonRegresar;
+
+    @FXML
+    private Button crearCuenta;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private VBox containerRight;
+
+    @FXML
+    void regresarInicio(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void CrearCuenta(ActionEvent event) {
+
+    }
+
+}
