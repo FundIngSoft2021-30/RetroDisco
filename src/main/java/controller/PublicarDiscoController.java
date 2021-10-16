0de3103001114baf8f5edca4a97f00435f60cee3
mejiarocha <70533112+mejiarocha@users.noreply.github.com>
2021-10-15 19:22:14 -0500

    Correcciones y pop ups en vender disco
    Correcciones de pequeños fallos y agregacion de mensajes emergentes a la hora de publicar un disco

--- src/main/java/controller/PublicarDiscoController.java
+++ src/main/java/controller/PublicarDiscoController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXMLLoader;
@@ -44,9 +45,9 @@ public class PublicarDiscoController {
 
     @FXML
     void VolverInicio(ActionEvent event) throws IOException {
-        Stage stage=(Stage) cancelar.getScene().getWindow();
+        Stage stage = (Stage) cancelar.getScene().getWindow();
         stage.close();
-        Parent root=FXMLLoader.load(getClass().getResource("../view/BuscarDisco.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/BuscarDisco.fxml"));
         stage.close();
         Scene scene = new Scene(root);
         stage.setTitle("Buscador - RetroDisco");
@@ -55,36 +56,55 @@ public class PublicarDiscoController {
     }
 
     @FXML
-    void publicarDisco(ActionEvent event) throws IOException {
-        if(cantidad.getText().isEmpty()||formato.getText().isEmpty()||genero.getText().isEmpty()||nombreartista.getText().isEmpty()||
-                nombredisco.getText().isEmpty()||precio.getText().isEmpty()||publicacion.getText().isEmpty()){
+    void publicarDisco(ActionEvent event) {
+
+        if (cantidad.getText().isEmpty() || formato.getText().isEmpty() || genero.getText().isEmpty()
+                || nombreartista.getText().isEmpty() || nombredisco.getText().isEmpty() || precio.getText().isEmpty()
+                || publicacion.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Alerta!");
+            alert.setHeaderText("Es necesario llenar todos los campos!");
+            alert.showAndWait();
             return;
-        }else{
+        } else {
             int cant = Integer.parseInt(cantidad.getText().trim());
             int pub = Integer.parseInt(publicacion.getText().trim());
             double pre = Double.parseDouble(precio.getText().trim());
             int cantidadDiscosUsuario = CRUD.obtenerDiscosUsuario(AppLauncher.getUsuarioActual().getUsername()).size();
-            String idDisco = "Disco-"+AppLauncher.getUsuarioActual().getUsername()+"-"+cantidadDiscosUsuario;
-            
-            Disco disco = new Disco(idDisco, nombredisco.getText().trim(), nombreartista.getText().trim(),pub, 
-                    formato.getText().trim(),pre,cant,AppLauncher.getUsuarioActual().getUsername(),genero.getText().trim());
-            System.out.println(disco.getArtista()+" - "+disco.getNombre()+" - "+disco.getId());
-            System.out.println("CANTIDAD: "+disco.getCantidad()+" - FORMATO: "+disco.getFormato()+" - GENERO: "+disco.getGenero());
-            System.out.println("VENDEDOR: "+disco.getVendedor()+" PRECIO: "+disco.getPrecio()+" PUBLICACION: "+disco.getPublicacion());
+            String idDisco = "Disco-" + AppLauncher.getUsuarioActual().getUsername() + "-" + cantidadDiscosUsuario;
+
+            Disco disco = new Disco(idDisco, nombredisco.getText().trim(), nombreartista.getText().trim(), pub,
+                    formato.getText().trim(), pre, cant, AppLauncher.getUsuarioActual().getUsername(),
+                    genero.getText().trim());
+            System.out.println(disco.getArtista() + " - " + disco.getNombre() + " - " + disco.getId());
+            System.out.println("CANTIDAD: " + disco.getCantidad() + " - FORMATO: " + disco.getFormato() + " - GENERO: "
+                    + disco.getGenero());
+            System.out.println("VENDEDOR: " + disco.getVendedor() + " PRECIO: " + disco.getPrecio() + " PUBLICACION: "
+                    + disco.getPublicacion());
             boolean b = CRUD.agregarDisco(disco);
-            if(b){
-                Stage stage=(Stage) publicar.getScene().getWindow();
+            if (b) {
+                Stage stage = (Stage) publicar.getScene().getWindow();
                 stage.close();
-                Parent root=FXMLLoader.load(getClass().getResource("../view/BuscarDisco.fxml"));
-                stage.close();
-                Scene scene = new Scene(root);
-                stage.setTitle("Buscador - RetroDisco");
-                stage.setScene(scene);
-                stage.show();
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("../view/BuscarDisco.fxml"));
+                    stage.close();
+                    Scene scene = new Scene(root);
+                    stage.setTitle("Buscador - RetroDisco");
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Alerta!");
+            alert.setHeaderText("Disco agruegado");
+            alert.showAndWait();
+            CRUD.agregarDisco(disco);
             return;
-            
         }
+
     }
 
 }
